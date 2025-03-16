5678fb401c9211e020529323f84a3c8e5160ebff
Stefano-Porrelli <133491983+Stefano-Porrelli@users.noreply.github.com>
2025-03-16 17:53:50 +0000

    02a_mtDNA_tree.sh

--- 02a_mtDNA_tree.sh
+++ 02a_mtDNA_tree.sh
@@ -23,14 +23,34 @@ cp "${DATA_DIR}"/mtDNA_MSA.nexus "${MITO_PHYLO}"
 cp "${DATA_DIR}"/mtDNA_partitions.txt "${MITO_PHYLO}"
 cd "$MITO_PHYLO" || exit
 
-# Infer mtDNA tree with concatenation method
-iqtree2 -s mtDNA_MSA.nexus -B 1000 --wbt -nt AUTO --prefix mtDNA_concatenated
+# Infer mtDNA tree with concatenation method (ML)
+iqtree2 -s mtDNA_MSA.nexus -B 1000 --wbt -nt AUTO --prefix MtDNA_concat
+cp ./MtDNA_concat.treefile ./MtDNA_concat.tree
 # Infer set of mtDNA genes/locus trees
-iqtree2 -s mtDNA_MSA.nexus -S mtDNA_partitions.txt -B 1000 --wbt -nt AUTO --prefix mtDNA_loci
+iqtree2 -s mtDNA_MSA.nexus -S mtDNA_partitions.txt -B 1000 --wbt -nt AUTO --prefix MtDNA_loci
+# compute gCF and sCF for the concatenated ML mitochondrial tree
+iqtree2 -t MtDNA_concat.treefile --gcf MtDNA_loci.treefile -s mtDNA_MSA.nexus --scf 100 --prefix MtDNA_concat_concord --cf-verbose --df-tree --cf-quartet
 
+# ASTRAL: Multispecies coalescent model (MSC)
+# Prepare input data files for ASTRAL
+ls MtDNA_loci.ufboot > loci_boot.txt
+
+# Run ASTRAL with multilocus bootstrap
+astral -i MtDNA_loci.treefile -b loci_boot.txt -r 1000 -o MtDNA_MSC_concord.trees
+tail -n 1 MtDNA_MSC_concord.trees > MtDNA_MSC_concord.tree
 # compute gCF and sCF 
-iqtree2 -t MtDNA_concatenated.treefile --gcf mtDNA_loci.treefile -s mtDNA_MSA.nexus --scf 100 --prefix MtDNA_concord --cf-verbose --df-tree --cf-quartet
+iqtree2 -t MtDNA_MSC_concord.tree --gcf MtDNA_loci.treefile -s mtDNA_MSA.nexus --scf 100 --prefix MtDNA_MSC_concord --cf-verbose --df-tree --cf-quartet
+
+# Run ASTRAL with local PP
+astral -i MtDNA_loci.treefile -o MtDNA_MSC_lpp_concord.tree
+# compute gCF and sCF 
+iqtree2 -t MtDNA_MSC_lpp_concord.tree --gcf MtDNA_loci.treefile -s mtDNA_MSA.nexus --scf 100 --prefix MtDNA_MSC_lpp_concord --cf-verbose --df-tree --cf-quartet
+
 # Cleanup
 mkdir -p ./trees
-mv ./*.treefile ./trees
 mv ./*.tree ./trees
+rm *.bionj
+rm *.ckp.gz
+rm *.mldist
+rm *.model.gz
+rm *.splits.nex
